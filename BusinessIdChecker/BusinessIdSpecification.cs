1a5f5b77ce17e0327d80fca709b3dc0064cbb2d1
ttpont <teppo.pontelin@gmail.com>
2016-08-29 21:35:24 +0300

    Initial commit

---
+++ BusinessIdChecker/BusinessIdSpecification.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace BusinessIdChecker
+{
+    public class BusinessIdSpecification : ISpecification<string>
+    {
+        private List<string> _reasonsForDissatisfaction;
+        private bool _isValidBusinessId;
+        private const string RegexPattern = @"^([0-9]{6,7})-([0-9]{1})";
+        private readonly int[] CheckSumMultiplyArray = new int[] { 7, 9, 10, 5, 8, 4, 2 };
+        private List<Action<string>> _checkList = new List<Action<string>>();
+
+        public BusinessIdSpecification()
+        {
+            _checkList.Add(LengthCheck);
+            _checkList.Add(RegexCheck);
+            _checkList.Add(ValidateCheckSum);
+        }
+
+        public IEnumerable<string> ReasonsForDissatisfaction
+        {
+            get
+            {
+                return _reasonsForDissatisfaction;
+            }
+        }
+
+        public bool IsSatisfiedBy(string entity)
+        {
+            _isValidBusinessId = true;
+            _reasonsForDissatisfaction = new List<string>();
+
+            if (entity == null)
+                entity = "";
+
+            foreach (var check in _checkList)
+            {
+                check(entity);
+            }
+
+            return _isValidBusinessId;
+        }
+
+        private void LengthCheck(string entity)
+        {
+            if (entity.Length < 8)
+            {
+                InvalidateBusinessId("Entity is too short.");
+                return;
+            }
+            else if (entity.Length > 9)
+            {
+                InvalidateBusinessId("Entity is too long.");
+                return;
+            }
+        }
+
+        private void RegexCheck(string entity)
+        {
+            var regex = new Regex(RegexPattern);
+            if (!regex.IsMatch(entity))
+            {
+                InvalidateBusinessId("Entity has invalid format.");
+            }
+        }
+
+        private void ValidateCheckSum(string entity)
+        {
+            try
+            {
+                if (entity.Length == 0)
+                {
+                    InvalidateBusinessId("Cannot calculate check sum for empty business ids.");
+                    return;
+                }
+
+                if (entity.Length > 9)
+                {
+                    InvalidateBusinessId("Cannot calculate check sum for too long business ids.");
+                    return;
+                }
+
+                var checkSum = (int)char.GetNumericValue(entity, entity.Length - 1);
+                int calculatedSum = 0;
+
+                // BusinessId can be 8 or 9 chars long. Add 0 prefix for too short businessIds
+                while (entity.Length < 8)
+                    entity = "0" + entity;
+
+                //for (int i = 0; i < entity.Length - 2; i++)
+                for (int i = 0; i < CheckSumMultiplyArray.Length; i++)
+                {
+                    calculatedSum += (int)char.GetNumericValue(entity, i) * CheckSumMultiplyArray[i];
+                }
+
+                var calculatedCheckDigit = calculatedSum % 11;
+
+                if (calculatedCheckDigit == 1)
+                {
+                    InvalidateBusinessId("Entity has invalid check sum. Check sum module cannot be 1.");
+                }
+
+                if (calculatedCheckDigit == 0 && checkSum != 0)
+                {
+                    InvalidateBusinessId("Entity has invalid check sum.");
+                }
+
+                if (calculatedCheckDigit > 2 && calculatedCheckDigit < 10 && checkSum != (11 - calculatedCheckDigit))
+                {
+                    InvalidateBusinessId("Entity has invalid check sum.");
+                }
+            }
+            catch
+            {
+                InvalidateBusinessId("Unspecified error while calculating check sum.");
+            }
+        }
+
+        private void InvalidateBusinessId(string message)
+        {
+            _reasonsForDissatisfaction.Add(message);
+            _isValidBusinessId = false;
+        }
+
+    }
+}
---
+++ BusinessIdCheckerTests/BusinessIdSpecificationsTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BusinessIdChecker;
+
+namespace BusinessIdCheckerTests
+{
+    [TestClass]
+    public class BusinessIdSpecificationsTests
+    {
+        private const string InvalidBusinessIdTooShort = "25779-2";
+        private const string InvalidBusinessIdTooLong = "25770999-2";
+        private const string InvalidBusinessIdFormat = "25770CB-2";
+        private const string ValidBusinessId = "2577099-2";
+        private const string ValidBusinessId2 = "2544890-1";
+        private const string ValidBusinessId3 = "2754676-7";
+        private const string ValidBusinessId4 = "1069622-4";
+
+        [TestMethod]
+        public void TestIsSatisfiedByAndReasonsForDissatisfaction()
+        {
+            var businessIdSpecification = new BusinessIdSpecification();
+            
+            // Too short
+            Assert.AreEqual(false, businessIdSpecification.IsSatisfiedBy(InvalidBusinessIdTooShort));
+            Assert.AreEqual(3, businessIdSpecification.ReasonsForDissatisfaction.Count());
+
+            // Too long
+            Assert.AreEqual(false, businessIdSpecification.IsSatisfiedBy(InvalidBusinessIdTooLong));
+            Assert.AreEqual(3, businessIdSpecification.ReasonsForDissatisfaction.Count());
+
+            //Invalid format
+            Assert.AreEqual(false, businessIdSpecification.IsSatisfiedBy(InvalidBusinessIdFormat));
+            Assert.AreEqual(2, businessIdSpecification.ReasonsForDissatisfaction.Count());
+
+            // Null
+            Assert.AreEqual(false, businessIdSpecification.IsSatisfiedBy(null));
+            Assert.AreEqual(3, businessIdSpecification.ReasonsForDissatisfaction.Count());
+
+            // Valid
+            Assert.AreEqual(true, businessIdSpecification.IsSatisfiedBy(ValidBusinessId));
+            Assert.AreEqual(0, businessIdSpecification.ReasonsForDissatisfaction.Count());
+
+            Assert.AreEqual(true, businessIdSpecification.IsSatisfiedBy(ValidBusinessId2));
+            Assert.AreEqual(0, businessIdSpecification.ReasonsForDissatisfaction.Count());
+
+            Assert.AreEqual(true, businessIdSpecification.IsSatisfiedBy(ValidBusinessId3));
+            Assert.AreEqual(0, businessIdSpecification.ReasonsForDissatisfaction.Count());
+
+            Assert.AreEqual(true, businessIdSpecification.IsSatisfiedBy(ValidBusinessId4));
+            Assert.AreEqual(0, businessIdSpecification.ReasonsForDissatisfaction.Count());
+        }
+    }
+}
